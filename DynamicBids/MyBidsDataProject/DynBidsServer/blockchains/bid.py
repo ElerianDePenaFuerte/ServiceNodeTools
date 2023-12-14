d21705be062d511ee047257f70d7407058412e42
Thomas <50805012+ThomasTestAccount@users.noreply.github.com>
2023-12-14 20:38:45 +0100

    Delete DynamicBids directory

--- DynamicBids/MyBidsDataProject/DynBidsServer/blockchains/bid.py
+++
@@ -1,9 +0,0 @@
-import dataclasses
-import logging
-
-
-@dataclasses.dataclass
-class Bid:
-    fee: int
-    execution_time: int
-    valid_until: int
--- DynamicBids/MyBidsDataProject/DynBidsServer/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export PYTHONPATH=""
-export PYTHONPATH="${PYTHONPATH}:$HOME/eclipse-workspace/DynBidsServer/"
-export PYTHONPATH="${PYTHONPATH}:$HOME/eclipse-workspace/DynBidsServer/ApiProvider"
-export PYTHONPATH="${PYTHONPATH}:$HOME/eclipse-workspace/DynBidsServer/blockchains"
-
-python3 ./ApiProvider/main.py
-
